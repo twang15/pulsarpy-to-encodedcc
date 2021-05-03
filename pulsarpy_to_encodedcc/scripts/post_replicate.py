c5469f88238688d45ff4ca8b5e188e552074d45d
Tao Wang <taowang9@stanford.edu>
2021-05-03 12:43:33 -0700

    develop code for patching a replicate, standalone

---
+++ pulsarpy_to_encodedcc/scripts/post_replicate.py
@@ -0,0 +1,58 @@
+import pulsarpy_to_encodedcc.dcc_submit as dcc_submit
+import traceback
+import argparse
+
+from encode_utils.parent_argparser import dcc_login_parser
+
+submitter = dcc_submit.Submit(dcc_mode='prod')
+#submitter = dcc_submit.Submit(dcc_mode='dev')
+#chipSeq = dcc_submit.Submit(dcc_mode='dev')
+
+def log_traceback(ex):
+    tb_lines = traceback.format_exception(ex.__class__, ex, ex.__traceback__)
+    for tb in tb_lines:
+        print(tb)
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+            description = __doc__,
+            parents=[dcc_login_parser],
+            formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument("-i", "--infile", required=True,
+            help="""
+      each row corresponds to one upstream_identifier of one Library.
+      """)
+
+    return parser
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    infile = args.infile
+  
+    fh = open(infile)
+    records = []
+    for line in fh:
+        line = line.strip()
+        if not line or line.startswith("#"):
+            continue
+  
+        records.append(line)
+  
+    fh.close()
+
+    fout = open('out', 'w')
+
+    for record in records:
+        # patch
+        upstream_id = submitter.post_replicate(pulsar_library_id=record, dcc_exp_id="ENCSR425FUS", patch=True)
+
+        # post
+        #upstream_id = submitter.post_replicate(pulsar_library_id=record, dcc_exp_id="ENCSR425FUS", patch=False)
+        fout.write(f'{record} {upstream_id}\n')
+    
+    fout.close()
+
+if __name__ == "__main__":
+  main()
